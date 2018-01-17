5957a7967243ab6e5b7cb5b94fc82dc26efe5cde
Jaco du Plessis <jaco@jacoduplessis.co.za>
2018-01-17 12:19:45 +0200

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='telegraph',
-    version='0.0.1',
+    version='0.0.2',
     packages=['telegraph'],
     include_package_data=True,
     install_requires=[
